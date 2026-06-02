933d79c09e220fc7e8072921b54f7642b50458d5
Wilder <wilder.tests@outlook.com>
2026-06-01 19:10:37 -0500

    Cambio de apellido

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-cicd-terraform-app-moreno" # Reemplazar por apellido
-    storage_account_name = "tfstatemoreno"                # Reemplazar por apellido
+    resource_group_name  = "rg-cicd-terraform-app-moreno1" # Reemplazar por apellido
+    storage_account_name = "tfstatemoreno1"                # Reemplazar por apellido
     container_name       = "tfstate"
     key                  = "dev/terraform.tfstate"
   }
